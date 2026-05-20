89cf4b1e2b51a2e7e19c9025ee9c38306ced4451
tranminhkhanggg2020-ctrl <tranminhkhanggg2020@gmail.com>
2026-05-20 17:09:32 +0700

    Create KryptonAura.java

---
+++ src/main/java/com/nnpg/glazed/modules/pvp/KryptonAura.java
@@ -0,0 +1,10 @@
+package com.nnpg.glazed.modules.pvp;
+
+import minegame159.meteorclient.systems.modules.Module;
+
+public class KryptonAura extends Module {
+    public KryptonAura() {
+        // Đăng ký thuộc nhóm Combat (PVP) của Meteor Client
+        super(minegame159.meteorclient.systems.modules.Categories.Combat, "krypton-aura", "Thuật toán Killaura tối ưu hóa từ Krypton Client.");
+    }
+}
